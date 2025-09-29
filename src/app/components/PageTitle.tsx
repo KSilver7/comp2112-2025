6b74a79435a1161c1ce3476db93611f599ac7f21
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-09-29 10:13:00 -0400

    Added a defined PrageTitleProps

--- src/app/components/PageTitle.tsx
+++ src/app/components/PageTitle.tsx
@@ -2,8 +2,12 @@
 
 import { useEffect } from "react";
 
+type PageTitleProps = {
+    title: string;
+}
+
 // component recieves a title prop as a string (unique on each page)
-export default function PageTitle({ title }: { title: string }) {
+export default function PageTitle({ title }: PageTitleProps ) {
     // trigger this effect and update the page title every time the title prop value changes
     // we need to use Effect Hook as layout only loads once. // this is the title at the top of the tab
     useEffect(() => {
