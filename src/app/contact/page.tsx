5a1d9f87110f047cffb9886cdd05818abb06c177
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-09-29 07:57:48 -0400

    fixed onclick error

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -25,13 +25,7 @@ export default function Contact() {
     /*I got an onClick event type error. The site still works, but the onClick is underlined. It only goes away
     if I change the event type from ChangeEvent and HTMLInput to MouseEvent and HTMLButtonElement */
     
-    /* My code:
-    const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>)
-    Suggested change from google ai from the search bar:
-    const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>)
-    But I left it as my own because it still works.
-    */
-    const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
         setConfirmation(`Your name: ${name} - Your message: ${message}`);
     }
 
